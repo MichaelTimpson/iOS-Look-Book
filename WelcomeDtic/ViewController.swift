fc67e39a3e28b31e4a46c55d99c09d11d46a444c
Michael <michael.d.timpson.ctr@mail.mil>
2018-10-31 19:09:32 -0400

    Initial Commit

---
+++ WelcomeDtic/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WelcomeDtic
+//
+//  Created by Timpson, Michael CTR DTIC P on 10/31/18.
+//  Copyright © 2018 Timpson, Michael CTR DTIC P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
