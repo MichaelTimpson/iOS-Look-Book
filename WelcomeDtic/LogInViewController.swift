10e864572bbdd66092c3485344aaad6f9bae3b9d
Michael <michael.d.timpson.ctr@mail.mil>
2019-01-09 16:59:06 -0500

    Add links to the social media icons/buttons

--- WelcomeDtic/LogInViewController.swift
+++ WelcomeDtic/LogInViewController.swift
@@ -19,6 +19,24 @@ class LogInViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func facebookButtonPressed(_ sender: UIButton) {
+        print("Facebook")
+        let facebookUrl = NSURL(string:"https://www.facebook.com/dtic.mil")! as URL
+        UIApplication.shared.open(facebookUrl, options: [:], completionHandler: nil)
+    }
+    @IBAction func twitterButtonPressed(_ sender: UIButton) {
+        let twitterUrl = NSURL(string:"https://www.twitter.com/doddtic?lang=en")! as URL
+        UIApplication.shared.open(twitterUrl, options: [:], completionHandler: nil)
+    }
+    @IBAction func linkedinButtonPressed(_ sender: UIButton) {
+        let linkedinUrl = NSURL(string:"https://www.linkedin.com/company/defense-technical-information-center")! as URL
+        UIApplication.shared.open(linkedinUrl, options: [:], completionHandler: nil)
+    }
+    
+    @IBAction func mailButtonPressed(_ sender: UIButton) {
+        print("Mail Button")
+    }
+    
     
     @IBAction func viewProductButtonPressed(_ sender: UIButton) {
         print("LogInViewControllerButtonPressed")
